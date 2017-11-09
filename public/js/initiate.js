b4ce9f1dd8e0779030b39c570da4c1817bce42c3
Sanjiv Krish <sanjiv436.k@gmail.com>
2017-11-09 19:29:58 +0100

    client: Include 'initiate.js'
    Initial script to load and manipulate data
    
    Signed-off-by: Sanjiv Krish <sanjiv436.k@gmail.com>

---
+++ public/js/initiate.js
@@ -0,0 +1,19 @@
+
+//
+// List of alphabets
+//
+var lettersList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+window.onload =  function() {
+
+  //
+  // Generate random number for position
+  //
+  var getRandomNumber = function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+};
