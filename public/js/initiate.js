d6fe77a0ade2b1ffeee0ef3753e43441f72a97bc
Sanjiv Krish <sanjiv436.k@gmail.com>
2017-11-14 19:01:22 +0100

    client: initiate: Include script to launch game
    Signed-off-by: Sanjiv Krish <sanjiv436.k@gmail.com>

--- public/js/initiate.js
+++ public/js/initiate.js
@@ -6,16 +6,6 @@ var lettersList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', '
                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 window.onload =  function() {
-
-  //
-  // Generate random number for position
-  //
-  /*var getRandomNumber = function (min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }*/
-
   //
   // Insert letter from the view
   //
@@ -43,7 +33,6 @@ window.onload =  function() {
     //
     var image = document.createElement("a-image");
     image.setAttribute('src', 'img/' + letter + '.png');
-    //image.setAttribute('scale', '0.5 0.5 0.5');
     image.setAttribute('scale', '1.5 1.5 1.5');
 
 
@@ -76,51 +65,55 @@ window.onload =  function() {
   //
   // Populate letters in the arena
   //
-  /*for (var i = 0; i < lettersList.length; i++) {
-    var x = getRandomNumber(-4, 4);
-    var y = getRandomNumber(-4, 4);
-    var z = getRandomNumber(-4, 4);
-    var position = x + ' ' + y + ' ' + z;
+  var populateLetters = function () {
+    let numberOfLetters = lettersList.length;
+    let numberOfIterations = numberOfLetters * 50;
+    let increment = 0;
+    let positions = new Set();
 
-    insertLetter(lettersList[i], position);
-  }*/
-   let numberOfLetters = lettersList.length;
-   let numberOfIterations = numberOfLetters * 50;
-   let increment = 0;
-   let positions = new Set();
+    do {
+      positions.add(generateRandomPosition())
+      increment ++;
+    } while(increment <= numberOfIterations);
 
-   do {
-     positions.add(generateRandomPosition())
-     increment ++;
-   } while(increment <= numberOfIterations);
+    function generateRandomPosition(){
+      let maxPositioning = numberOfLetters * 3;
+      let minPositioning = 20;
+      return Math.floor(Math.random()* maxPositioning)-minPositioning;
+    }
 
-   function generateRandomPosition(){
-     let maxPositioning = numberOfLetters * 3;
-     let minPositioning = 20;
-     return Math.floor(Math.random()* maxPositioning)-minPositioning;
-   }
+    let uniquePositions = Array.from(positions);
+    let coords = [];
+    while(uniquePositions.length) coords.push(uniquePositions.splice(0,3));
 
-   let uniquePositions = Array.from(positions);
-   let coords = [];
-   while(uniquePositions.length) coords.push(uniquePositions.splice(0,3));
-
-   coords.forEach((axis, index) => {
-     let position = axis[0] + ' ' + axis[1] + ' ' + axis[2];
-     insertLetter(lettersList[index], position);
-   });
-
-   //End of random positioning
-
-
-  if (annyang) {
-  var commands = {
-    'letter *let': removeLetter
+    coords.forEach((axis, index) => {
+      let position = axis[0] + ' ' + axis[1] + ' ' + axis[2];
+      insertLetter(lettersList[index], position);
+    });
   };
 
-  // Add our commands to annyang
-  annyang.addCommands(commands);
+  //
+  // Begin game
+  //
+  var startGame = function () {
+    document.getElementById('container').outerHTML = '';
+    document.getElementsByTagName('a-scene')[0].style.zIndex = 'auto';
+    populateLetters();
+  };
 
-  // Start listening. You can call this here, or attach this call to an event, button, etc.
-  annyang.start();
-}
+  //
+  // Receive voice input
+  //
+  if (annyang) {
+    var commands = {
+      'letter *let': removeLetter,
+      'startGame': startGame
+    };
+
+    // Add our commands to annyang
+    annyang.addCommands(commands);
+
+    // Start listening. You can call this here, or attach this call to an event, button, etc.
+    annyang.start();
+  }
 };
