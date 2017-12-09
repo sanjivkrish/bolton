054740ea7a9bd11c7bf6089a563bd176c311c678
Sanjiv Krish <sanjiv436.k@gmail.com>
2017-12-10 00:02:49 +0100

    Provide an img url to eliminate favicon bug
    Signed-off-by: Sanjiv Krish <sanjiv436.k@gmail.com>

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
-// app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(favicon(__dirname + '/public/img/bg.jpg'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
